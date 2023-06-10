f8c2ebb8749270e7fe0b6356870ceb9719f59482
kp-156 <72290998+kp-156@users.noreply.github.com>
2023-06-10 17:14:57 +0000

    Added dynamic data and api route

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+QUIZZES = [
+  {
+    'id': 1,
+    'title': 'What kind of potato are you?'
+    
+  },
+  {
+    'id': 2,
+    'title': 'What your favorait potato dish thinks of you'
+    
+  },
+  {
+    'id': 3,
+    'title': 'How many potatos do you think it cost to build a potato castle'
+    
+  },
+  {
+    'id': 4,
+    'title': 'How many potatoes will it take to defeat The Rock'
+    
+  }
+]
 
 @app.route("/")
-def hello_world():
-  return render_template('home.html')
-  
+def hello_potatoes():
+  return render_template('home.html', quizzes = QUIZZES)
+
+@app.route("/api/quizzes")
+def list_quiz():
+  return jsonify(QUIZZES)
 
 if __name__ == '__main__':
   app.run(host = '0.0.0.0', debug = True)
